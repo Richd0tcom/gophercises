7fb7201d9a1bc0c25d607e5c8f6c26f1d71e36cc
Richd0tcom <tuberich0@gmail.com>
2023-01-10 17:22:28 +0100

    feat(url-shortener): add yaml handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module gophercises
+module github.com/Richd0tcom/gophercises
 
 go 1.19
+
+require gopkg.in/yaml.v2 v2.4.0
--- url-shortener/handler.go
+++ url-shortener/handler.go
@@ -1,34 +1,17 @@
-package shortner
-
+package shortener
 
 import (
 	"net/http"
-
 	yaml "gopkg.in/yaml.v2"
 )
 
 
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		if dest, ok := pathsToUrls[path]; ok {
-			http.Redirect(w, r, dest, http.StatusFound)
-			return
-		}
-		fallback.ServeHTTP(w, r)
-	}
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
 
-
-func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	pathUrls, err := parseYaml(yamlBytes)
-	if err != nil {
-		return nil, err
-	}
-	pathsToUrls := buildMap(pathUrls)
-	return MapHandler(pathsToUrls, fallback), nil
-}
-
+//converts the data from the array of urls into a map
 func buildMap(pathUrls []pathUrl) map[string]string {
 	pathsToUrls := make(map[string]string)
 	for _, pu := range pathUrls {
@@ -37,6 +20,7 @@ func buildMap(pathUrls []pathUrl) map[string]string {
 	return pathsToUrls
 }
 
+//parses the data from the yaml file into an array of url and corresponding paths
 func parseYaml(data []byte) ([]pathUrl, error) {
 	var pathUrls []pathUrl
 	err := yaml.Unmarshal(data, &pathUrls)
@@ -46,7 +30,47 @@ func parseYaml(data []byte) ([]pathUrl, error) {
 	return pathUrls, nil
 }
 
-type pathUrl struct {
-	Path string `yaml:"path"`
-	URL  string `yaml:"url"`
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+	
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(ymlData []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(ymlData)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
 }
